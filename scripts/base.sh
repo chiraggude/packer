3ff694bdf8e7a5c912fa267cbab2f1cd6a9a01c1
Chirag Gude <chiraggude@gmail.com>
2014-06-08 20:34:15 +0100

    Added comments

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
-# Download and install EPEL repo and then remove the rpm file
+# Download and install EPEL repo
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
+
+# Remove the EPEL rpm file
 rm -rf epel-release-*.rpm
 
 # Install Dev Tools for VirtualBox Guest Additions
--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+# Install Puppet
 wget http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
 rpm -ivh puppetlabs-release-6-10.noarch.rpm
 yum -y install puppet
